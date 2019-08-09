5f6c14a188bc218656733627c8ea338e6905ba23
rohan <rohan@ownedoutcomes.com>
2019-08-09 10:17:45 -0700

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <Home></Home>
+  </div>
+</template>
+
+<script>
+
+import Header from './components/Header'
+import Home from './components/Home'
+
+export default {
+  name: 'app',
+  components: {
+    Header,
+    Home
+  }
+}
+</script>
+
+<style>
+#app {
+  /* text-align: center */
+  font-family: 'Questrial';
+}
+</style>
